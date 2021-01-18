b9db369ac665f2595e17b797317251964f958653
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2021-01-18 23:21:23 +0100

    ✨ Implements findAll todos and create todo.

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,9 +1,20 @@
-import { Resolver, Query } from '@nestjs/graphql'
+import { Resolver, Query, Args, Mutation, ID } from '@nestjs/graphql'
+
+import { TodoService } from './todo.service'
+import { TodoType } from './type/todo.type'
+import { TodoInput } from './inputs/todo.input'
 
 @Resolver()
 export class TodoResolver {
-  @Query(() => String)
+  constructor(private readonly todoService: TodoService) {}
+
+  @Query(() => [TodoType])
   async todos() {
-    return 'hello from Essen!'
+    return this.todoService.findAll()
+  }
+
+  @Mutation(() => TodoType)
+  async createTodo(@Args('input') input: TodoInput) {
+    return this.todoService.create(input)
   }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,6 +1,28 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
+
+import { TodoDoc } from './interfaces/todo.interface'
+import { TodoInput } from './inputs/todo.input'
+import { TodoType } from './type/todo.type'
 
 @Injectable()
 export class TodoService {
-  constructor() {}
+  constructor(@InjectModel('Todo') private todoModel: Model<TodoDoc>) {}
+
+  async findAll(): Promise<TodoType[]> {
+    try {
+      return await this.todoModel.find().exec()
+    } catch {
+      throw new InternalServerErrorException(`Back later we will fix it.`)
+    }
+  }
+
+  async create(input: TodoInput): Promise<TodoType> {
+    if (input.title) {
+      return await this.todoModel.create(input)
+    } else {
+      throw new BadRequestException(`todo title missed.`)
+    }
+  }
 }
