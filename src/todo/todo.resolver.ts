94302ae91a5fe200ab0698325adc12309a9905f7
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2021-01-18 23:30:23 +0100

    ✨ Updates todo status.

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -3,6 +3,7 @@ import { Resolver, Query, Args, Mutation, ID } from '@nestjs/graphql'
 import { TodoService } from './todo.service'
 import { TodoType } from './type/todo.type'
 import { TodoInput } from './inputs/todo.input'
+import { Status } from './enum/status.enum'
 
 @Resolver()
 export class TodoResolver {
@@ -16,5 +17,10 @@ export class TodoResolver {
   @Mutation(() => TodoType)
   async createTodo(@Args('input') input: TodoInput) {
     return this.todoService.create(input)
+  }
+
+  @Mutation(() => TodoType)
+  async updateStatus(@Args('id') id: string, @Args('status') status: Status) {
+    return this.todoService.updateStatus(id, status)
   }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -5,6 +5,8 @@ import { Model } from 'mongoose'
 import { TodoDoc } from './interfaces/todo.interface'
 import { TodoInput } from './inputs/todo.input'
 import { TodoType } from './type/todo.type'
+import { Status } from './enum/status.enum'
+import { checkStatus } from './../../utils/check-status'
 
 @Injectable()
 export class TodoService {
@@ -23,6 +25,28 @@ export class TodoService {
       return await this.todoModel.create(input)
     } else {
       throw new BadRequestException(`todo title missed.`)
+    }
+  }
+
+  async updateStatus(id: string, status: Status): Promise<TodoType> {
+    if (checkStatus(status)) {
+      try {
+        const chosenTodo = await this.todoModel.findByIdAndUpdate(
+          id,
+          {
+            $set: {
+              status
+            }
+          },
+          { new: true }
+        )
+
+        return chosenTodo
+      } catch {
+        throw new NotFoundException(`todo not exist.`)
+      }
+    } else {
+      throw new BadRequestException(`Status must be TODO or DONE.`)
     }
   }
 }
